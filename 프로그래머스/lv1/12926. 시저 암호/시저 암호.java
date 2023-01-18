5331650ae3fc47d367a0d8597521f7195d72025b
donghyeon <ghehdgus1128@gmail.com>
2023-01-18 21:24:17 +0900

    [level 1] Title: 시저 암호, Time: 32.99 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
@@ -1,22 +1,14 @@
 class Solution {
     public String solution(String s, int n) {
         String answer = "";
-        int i, sLen = s.length(), sCharAscii, sCharPlusN;
+        int i, sLen = s.length(), sCharAscii;
         for(i = 0; i < sLen; i++) {
         	// 알파벳 판단
         	sCharAscii = (int)s.charAt(i);
         	if(65 <= sCharAscii && sCharAscii <= 90) {// A-Z : 65-90
-        		sCharPlusN = sCharAscii + n;
-        		if(sCharPlusN > 90) {
-        			sCharPlusN -= 26;
-        		}
-        		answer += new Character((char)sCharPlusN).toString(); 
+        		answer += (char)('A' + (s.charAt(i) + n - 'A') % 26);
         	} else if(97 <= sCharAscii && sCharAscii <= 122) {// a-z : 97-122
-        		sCharPlusN = sCharAscii + n;
-        		if(sCharPlusN > 122) {
-        			sCharPlusN -= 26;
-        		}
-        		answer += new Character((char)sCharPlusN).toString();
+        		answer += (char)('a' + (s.charAt(i) + n - 'a') % 26);
         	} else answer += s.charAt(i);
 //        	System.out.format("%d : %d\n",i,sCharAscii);
         }
