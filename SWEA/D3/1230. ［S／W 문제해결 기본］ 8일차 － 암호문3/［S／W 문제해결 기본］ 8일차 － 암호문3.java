1ec6c56492abf94f2413d64ee603348e808d2293
donghyeon <ghehdgus1128@gmail.com>
2023-05-20 11:19:37 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문3, Time: 415 ms, Memory: 83,556 KB -BaekjoonHub

---
+++ SWEA/D3/1230. ［S／W 문제해결 기본］ 8일차 － 암호문3/［S／W 문제해결 기본］ 8일차 － 암호문3.java
@@ -0,0 +1,117 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+import java.io.FileInputStream;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		/*
+		   아래의 메소드 호출은 앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
+		   여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
+		   이 코드를 프로그램의 처음 부분에 추가하면 이후 입력을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
+		   따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 메소드를 사용하셔도 좋습니다.
+		   단, 채점을 위해 코드를 제출하실 때에는 반드시 이 메소드를 지우거나 주석 처리 하셔야 합니다.
+		 */
+		//System.setIn(new FileInputStream("res/input1230.txt"));
+
+		/*
+		   표준입력 System.in 으로부터 스캐너를 만들어 데이터를 읽어옵니다.
+		 */
+		Scanner sc = new Scanner(System.in);
+		int N, i, j, commandLen, x, y;
+		LinkedList<Integer> origin;
+		char command;
+		
+		/*
+		   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+		*/
+
+		for(int test_case = 1; test_case <= 10; test_case++)
+		{
+			/*
+			 * 첫 번째 줄 : 원본 암호문의 길이 N ( 10 ≤ N ≤ 20 의 정수)
+
+				두 번째 줄 : 원본 암호문
+				
+				세 번째 줄 : 명령어의 개수 ( 5 ≤ N ≤ 10 의 정수)
+				
+				네 번째 줄 : 명령어
+			 */
+			try {
+				N=sc.nextInt();
+				origin = new LinkedList<Integer>();
+				for(i = 0; i < N; i++) {
+					origin.add(sc.nextInt());
+				}
+				commandLen = sc.nextInt();
+				for(i = 0; i < commandLen; i++) { // 명령어 실행
+					command = sc.next().charAt(0);
+					x = sc.nextInt(); // x번째 위치
+					if(command == 'A') { // 맨 뒤에 추가
+						for(j = 0; j < x; j++) { // 맨 뒤에 x만큼 삽입
+							origin.offer(sc.nextInt());
+						}
+					} else {
+						y = sc.nextInt(); // 삽입 개수 y
+						if(command == 'I') { // 삽입
+							for(j = 0; j < y; j++) { // x 위치에 삽입
+								origin.add(x++, sc.nextInt());
+							}
+						} else if(command == 'D') { // 삭제
+							for(j = 0; j < y; j++) { // x 위치에 삽입
+								origin.remove(x);
+							}
+						}
+					}
+//					if(test_case == 6) {						
+//						System.out.print("#####"+N+" ");
+//						System.out.println(origin);
+//					}
+				}
+				System.out.format("#%d",test_case);
+				for(i = 0; i < 10; i++) {
+					System.out.format(" %d", origin.get(i));
+				}
+				System.out.println();
+			} catch (Exception e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+			
+		}
+	}
+}
